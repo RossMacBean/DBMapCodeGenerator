68d374c36dc6438aebf4a0e03fbbac4cb722535b
RossMacBean <RossMacBean@users.noreply.github.com>
2019-12-13 19:35:59 +0000

    Remove username and password from old soure

--- RPCGenerator/src/RPCGenerator.java
+++ RPCGenerator/src/RPCGenerator.java
@@ -3,11 +3,10 @@ import procedureGenerator.ProcedureGenerator;
 
 public class RPCGenerator {
 
-	//TODO:: load this from user input
 	static String schemaName = "sakila";
 	static String connectionString = "jdbc:mysql://localhost:3306/";
-	static String userName = "root";
-	static String password = "password";
+	static String userName = "";
+	static String password = "";
 	
 	public static void main(String[] args) {	
 		ProcedureGenerator pg = new ProcedureGenerator();
